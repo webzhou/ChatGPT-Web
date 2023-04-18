2c4f9a1e173642013e6d0841292086f605f4de74
study <773827742@qq.com>
2023-04-18 13:46:27 +0800

    Update GithubSite.vue

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="text-neutral-400">
-    <span>Star on</span>
-    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
-      GitHub
+    <span>Visit on</span>
+    <a href="https://www.mla.xin" target="_blank" class="text-blue-500">
+      Mla.xin
     </a>
   </div>
 </template>
